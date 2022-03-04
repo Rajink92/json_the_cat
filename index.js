56050898c806ab9d9f2b47c5a7dd856b57051962
Rajin <rajink92@gmail.com>
2022-03-04 17:02:30 -0500

    added processargv

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const { fetchBreedDescription } = require('./breedFetcher');
 const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
-  if (error) {
-    console.log('Error fetch details:', error);
-  } else {
-    console.log(desc);
-  }
+  if (process.argv.length > 2) {
+    process.argv.slice(2).forEach(breed =>
 });
